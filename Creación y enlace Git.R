a6e280fbe2a3ba35e9e64a8eeea02bf110be432e
Camila-Ormachea <a20220717@pucp.edu.pe>
2026-06-17 17:06:21 -0500

    Initial commit

---
+++ Creación y enlace Git.R
@@ -0,0 +1,9 @@
+#==============================================================================
+#Proyecto: Análisis usando datos de la ENAHO
+#Autora: Camila Ormachea
+#Objetivo de este script: Crear el sistema de carpetas y enlazar con GitHub
+#==============================================================================
+
+#Creamos carpetas-----------------------
+
+dir

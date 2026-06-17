18965aa696751545fece89ab8aafba213ff0f01b
Camila-Ormachea <a20220717@pucp.edu.pe>
2026-06-17 18:33:39 -0500

    Creamos script para carga y unión de módulos de ENAHO

---
+++ scripts/01_Carga_union_modulos.R
@@ -0,0 +1,13 @@
+#==============================================================================
+#Proyecto: Análisis usando la ENAHO
+#Script: Cargar los módulos y hacer los joints
+#Autora: Camila Ormachea
+#Fecha: 17-06-2026
+#==============================================================================
+
+#1.Carga de librerías------------------------------------------------------
+library(rio)
+library(tidyverse)
+library(janitor)
+library(readr)
+
